d637b1ca8341514c900c42fc813e3b0f867acf3e
jithinTB <jithintb805@gmail.com>
2025-09-16 12:09:56 +0530

    update pages

--- js/custom.js
+++ js/custom.js
@@ -33,12 +33,4 @@ $('.slick_slider_nav').slick({
 });
 
 
-/** google_map js **/
-
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+// End of custom js
